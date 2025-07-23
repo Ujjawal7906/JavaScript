7c0ec5a9718f955460de1809f4b5e52b04d2b218
Ujjawal Thakur <ujjawalthakur896@gmail.com>
2025-07-23 14:01:06 +0530

    Datatypes in JS

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+let name = "Ujjawal Thakur"; //srting
+let age = 20;  //numeric
+let isLoggedIn = true;  //boolean
+let city;//undefined
+
+console.table([typeof name, typeof age, typeof isLoggedIn, typeof city, typeof undefined, typeof null]);
+
+
+//type of null is object
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
