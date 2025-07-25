0b09918654a5c0157c0bead44d287eefd1d35c2d
Ujjawal7906 <ujjawalthakur896@gmail.com>
2025-07-25 16:45:14 +0530

    Comparison in JS

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -4,7 +4,7 @@
 // console.log(2 == 1);
 // console.log(2 != 1);//not equal to
 
-// console.log("2" > 1);
+// console.log("2" > 1); 
 // console.log("02" > 1);//string is convert into number then compare
 
 // console.log(null > 0);
@@ -13,8 +13,6 @@
 
 // console.log(undefined == 0);
 // console.log(undefined > 0);
-// console.log(undefined < 0);
-
 // === 
-
 console.log("2" === 2);
+// console.log(undefined < 0);
