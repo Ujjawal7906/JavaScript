c7cfc1412617911ec6dd36ae7c14211734a4de65
Ujjawal7906 <ujjawalthakur896@gmail.com>
2025-07-29 20:32:32 +0530

    Variables in JS

---
+++ 02_Variables.js
@@ -0,0 +1,14 @@
+var a = 67 // a contains 67
+console.log(a)
+a = "ujjawal"
+console.log(a)
+
+// Rules of Variables
+
+// let 8harry = 7 // Not allowed this will throw an error
+// let var = 7  //Not allowed because var is a reserved keyword
+// let Name = "Ujjawal" 
+// let name = "Thakur" //Both are different variables
+// console.log(Name);
+// console.log(name);
+
