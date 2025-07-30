a5ff2480ca011488b3ee49ca87ab59256d7468ad
Ujjawal7906 <ujjawalthakur896@gmail.com>
2025-07-30 20:43:59 +0530

    Operators in JS

---
+++ 04_operators.js
@@ -0,0 +1,51 @@
+// Arithematic Operators
+
+let a = 9;
+let b = 4;
+
+console.log("a + b = ", a + b);  // 13
+console.log("a - b = ", a - b);  // 5
+console.log("a * b = ", a * b);  // 36
+console.log("a / b = ", a / b);  // 2.25
+console.log("a % b = ", a % b);  // 1
+console.log("a ** b = ", a ** b);  // 6561
+console.log("a++ = ", a++);  // 9
+console.log("a = ", a);  // 10
+console.log("++a = ", ++a);  // 11
+console.log("a-- = ", a--);  // 11
+console.log("a = ", a);  // 10
+console.log("--a = ", --a);  // 9
+
+// Assignment Operators
+
+let x = 1;
+
+x += 4;
+console.log("x is now ", x);  // 5
+x -= 1;
+console.log("x is now ", x);  //4
+x *= 6;
+console.log("x is now ", x);  //24
+x /= 2;
+console.log("x is now ", x);  // 12 
+
+// Comparison Operator
+let num1 = 5;
+let num2 = "5";
+
+console.log(num1);
+console.log(num2);
+console.log("num1 == num2 is", num1 == num2);  // true
+console.log("num1 != num2 is", num1 != num2);  // false
+console.log("num1 === num2 is", num1 === num2);  // false because === also check data type
+console.log("num1 !== num2 is", num1 !== num2);  // true
+console.log("num1 > num2 is", num1 > num2);  // false
+console.log("num1 < num2 is", num1 < num2);  // false
+
+// Comments in JS
+// This is Single line Comment
+/* This is
+double line
+comment*/
+
+
