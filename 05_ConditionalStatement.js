a1ca938d928e4dfd9a049190947dbacb73c292df
Ujjawal7906 <ujjawalthakur896@gmail.com>
2025-07-31 17:18:37 +0530

    CONDITIONAL STATEMENTS IN JS

---
+++ 05_ConditionalStatement.js
@@ -0,0 +1,27 @@
+// Conditional Statements
+
+// Case 1
+
+let age = 11;
+
+if (age<18) {
+    console.log("You are not eligible for drive");
+}
+
+else{
+    console.log("You are eligible for drive");
+}
+
+// Case 2
+
+let marks = 46;
+
+if(marks <= 33){
+    console.log("Third Division");
+}
+else if(marks > 33 && marks <= 75){
+    console.log("Second Division");
+}
+else{
+    console.log("First Division");
+}
