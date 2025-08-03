7c4074fe44afcfda559144b819d6deabc33bf398
Ujjawal7906 <ujjawalthakur896@gmail.com>
2025-08-03 21:35:36 +0530

    Strings in JS

---
+++ 08_strings.js
@@ -0,0 +1,13 @@
+// Double Quote string
+let name = "Ujjawal Thakur";
+console.log(name);
+console.log(name.length);
+
+// Single Quote string
+let myName = 'Ujjawal Thakur';
+console.log(myName);
+console.log(myName.length);
+
+// let myName = "Ujjawal Thakur';  // This is not acceptable
+// console.log(myName);
+
