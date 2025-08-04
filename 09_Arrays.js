38ec8888f6df37c2396ea08a709945ff889e753d
Ujjawal7906 <ujjawalthakur896@gmail.com>
2025-08-04 19:25:28 +0530

    Arrays in JS

---
+++ 09_Arrays.js
@@ -0,0 +1,14 @@
+let marks = [66, 56, 87, 95, 44, "not present", null, false];
+console.log(marks);
+// console.log(marks[0]);
+// console.log(marks[1]);
+// console.log(marks[2]);
+// console.log(marks[3]);
+// console.log(marks[4]);
+// console.log(marks[5]);
+// console.log(marks[6]);
+// console.log(marks[7]);
+// console.log(marks[8]);
+ for(let i = 0; i < marks.length; i++){
+    console.log(marks[i]);
+ }
