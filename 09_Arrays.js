8809f8d61593424cc1dbb8ec89fdcf7e61952706
Ujjawal7906 <ujjawalthakur896@gmail.com>
2025-08-05 22:03:17 +0530

    Arrays in JS

--- 09_Arrays.js
+++ 09_Arrays.js
@@ -9,6 +9,26 @@ console.log(marks);
 // console.log(marks[6]);
 // console.log(marks[7]);
 // console.log(marks[8]);
- for(let i = 0; i < marks.length; i++){
-    console.log(marks[i]);
- }
+//  for(let i = 0; i < marks.length; i++){
+//     console.log(marks[i]);
+//  }
+
+// Array Method in JS
+let colors = ["Red", "Blue", "Green"];
+console.log(colors);
+console.log(colors.length);
+colors[4] = "Yellow";
+console.log(colors);
+
+let a = colors.join(" and ");
+console.log(a);
+
+let b = colors.pop(); // pop returns the popped element
+console.log(colors);
+
+let c = colors.push("Black");
+console.log(colors);
+
+
+
+
